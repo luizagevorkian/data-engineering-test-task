145099e1f14a94b39da296d88db065298efeabd4
luizagevorkian <66139931+luizagevorkian@users.noreply.github.com>
2025-09-12 22:41:12 +0200

    update

--- src/main.py
+++ src/main.py
@@ -2,37 +2,24 @@ from extract import extract_data
 from transform import transform_data
 from load import load_to_db
 from analytics import run_queries
-import pandas as pd
-from sqlalchemy import create_engine
-import json
-from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
-with open("data/raw/2025-09-12/response.json", "r") as f:
-    raw_data = json.load(f)
-
-df = pd.DataFrame(raw_data)
-
-# 2. We process data
-df = df[['id', 'title', 'body']]  # leave only the necessary fields
-df = df.dropna()  # delete empty entries
-
-# 3. connect to SQLite
-engine = create_engine('sqlite:///data/local.db')
-
-# 4. Save the DataFrame to the “posts” table
-df.to_sql('posts', con=engine, if_exists='replace', index=False)
-
-print("Данные успешно загружены в SQLite!")
-
 if __name__ == "__main__":
-    print("Starting mini ETL pipeline...")
+    print(" Starting mini ETL pipeline...")
+
+    # 1. Extract
     raw = extract_data()
+
+    # 2. Transform
     processed_df = transform_data(raw)
+
+    # 3. Load
     load_to_db(processed_df)
+
+    # 4. Analytics
     report = run_queries()
+
     print("Pipeline completed successfully!")
     print("Analytics Results:", report)
-
