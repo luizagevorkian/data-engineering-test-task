d89fe8b1c3e4d0681710541dadb3d13da0cd5337
luizagevorkian <66139931+luizagevorkian@users.noreply.github.com>
2025-09-13 12:01:11 +0200

    Update

--- src/load.py
+++ src/load.py
@@ -5,11 +5,11 @@ import os
 def load_to_db(df, table_name="posts"):
     """
     Load processed data to a database using SQLAlchemy.
-    DATABASE_URL читается из переменной окружения.
+    DATABASE_URL read from an environment variable.
     
-    Пример DATABASE_URL:
-    - SQLite (локально): sqlite:///local.db
-    - PostgreSQL (в Docker): postgresql+psycopg2://postgres:postgres@db:5432/postgres
+    DATABASE_URL:
+    - SQLite (local): sqlite:///local.db
+    - PostgreSQL (in Docker): postgresql+psycopg2://postgres:postgres@db:5432/postgres
     """
     db_url = os.getenv("DATABASE_URL", "sqlite:///local.db")
     engine = create_engine(db_url)
